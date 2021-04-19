c2521f01e4f59d82f52e7bc786e5af390dc54898
olaf kuras <kurasolaf@gmail.com>
2021-04-19 23:17:14 +0200

    Made random name generator for TEST Practice

---
+++ Models/Enums/WeaponNames.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgChest
+{
+    class WeaponNames
+    {
+        enum WeaponAdjectiveEnum
+        {
+
+            Holy = 1,
+            Cursed = 2,
+            Evil = 3,
+            Fast = 4,
+            Greedy = 5
+
+        }
+
+        enum WeaponNounEnum
+        {
+            Steel = 1,
+            Bone = 2,
+            Hand = 3,
+            Soul = 4,
+            Wanderer = 5
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Services/NameGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RpgChest.ItemChoose;
+
+namespace RpgChest
+{
+    class NameGenerator
+    {
+        public string WeaponNameGenerator(WeaponsItemChoose type)
+        {
+            WeaponsItemChoose itType = type;
+            string name;
+
+            string adjs;
+            string nouns;
+            //make adjective field
+            //make noun field
+
+            //assign noun random method with noun field
+            //assign adj random method with adj field
+
+
+
+
+            name = $"{itType} of ";
+
+
+            return name;
+
+        }
+
+        // make private method for noun random
+        //make private method for adjective random
+
+
+
+
+
+
+
+
+    }
+}
