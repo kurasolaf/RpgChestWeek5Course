ea9512a46b5a57e5c8ed4d1339db060451e90150
olaf kuras <kurasolaf@gmail.com>
2021-05-21 23:17:00 +0200

    added elements

---
+++ RpgChestWeek4/Helpers/PdfFiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RpgChest.Helpers
+{
+    public class PdfFiles
+    {
+        public void DownloadCharacterInventoryFile()
+        {
+            var inventory = File.OpenRead(@"C:\Users\kuras\OneDrive\Pulpit\DnD\KartaPostaci1.4v.png");
+
+        }
+
+    }
+
+
+
+
+
+}
