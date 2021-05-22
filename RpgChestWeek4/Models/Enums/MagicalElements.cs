0592553cadba803ffc36bf6f3295ee177681039a
olaf kuras <kurasolaf@gmail.com>
2021-05-22 20:14:49 +0200

    Added basic and advanced elements

--- RpgChestWeek4/Models/Enums/MagicalElements.cs
+++ RpgChestWeek4/Models/Enums/MagicalElements.cs
@@ -8,18 +8,34 @@ namespace RpgChest.Models.Enums
 {
     class MagicalElements
     {
-        enum elements
+        enum BasicElements
         {
             Water = 1,
             Fire = 2,
-
-
-
-
-
-
+            Wind = 3,
+            Earth = 4,
+            Energy = 5,
+            Holy = 6,
+            IllusionAndEnchanting = 7,
+            Curses = 8,
+            Nature = 9,
+            Necromancy = 10
+            
         }
 
+        enum AdvancedElements
+        {
+            Ice = 1,
+            Lava = 2,
+            Iron = 3,
+            Gravity = 4,
+            Acid = 5,
+            Gas = 7,
+            AdvancedElement8 = 8,
+            AdvancedElement9 = 9,
+            AdvancedElement10 = 10,
+
+        }
 
 
 
